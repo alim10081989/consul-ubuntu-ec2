95ac7b8aedff9523aa93f85768865ad17c851a8a
alim10081989 <alim.azad@gmail.com>
2022-03-03 10:34:55 +0530

    Added template for consul cluster

--- main.tf
+++ main.tf
@@ -1,17 +1,39 @@
-resource "aws_instance" "consul" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  count         = 2
-  key_name      = "AWS"
-  vpc_security_group_ids = [ "sg-0f9c3281b55a4d38f" ]
+resource "aws_instance" "consul_server" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  key_name               = "AWS"
+  vpc_security_group_ids = ["sg-0f9c3281b55a4d38f"]
 
   tags = {
-    Name = "consul-${count.index}"
+    Name = "consul-server"
   }
 
-  user_data = data.template_file.consul.rendered
+  user_data = data.template_file.consul_server.rendered
 }
 
-data "template_file" "consul" {
-  template = file("templates/consul_install.tpl")
+
+data "template_file" "consul_server" {
+  template = file("templates/consul_server/consul_install.tpl")
+}
+
+resource "aws_instance" "consul_client" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  key_name               = "AWS"
+  count                  = 2
+  vpc_security_group_ids = ["sg-0f9c3281b55a4d38f"]
+
+  tags = {
+    Name = "consul-client-${count.index + 1}"
+  }
+
+  user_data = data.template_file.consul_client.rendered
+}
+
+
+data "template_file" "consul_client" {
+  template = file("templates/consul_client/consul_install.tpl")
+  vars = {
+    tpl_server_private_ip = aws_instance.consul_server.private_ip
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,26 @@
-output "consul_server_details" {
-    value = <<EOT
+output "consul_server" {
+  value = <<EOT
  
-    Private IP : ${aws_instance.consul[0].private_ip}  
-    $ ssh -i ${aws_instance.consul[0].key_name}.pem ubuntu@${aws_instance.consul[0].public_ip}
-
-    Start Consul in Dev mode via CLI: 
-    
-    consul agent -dev -client=0.0.0.0 -bind=${aws_instance.consul[0].private_ip} -hcl 'disable_remote_exec=false'
+    Private IP : ${aws_instance.consul_server.private_ip}  
+    $ ssh -i ${aws_instance.consul_server.key_name}.pem ubuntu@${aws_instance.consul_server.public_ip}
 
     EOT
 }
 
-output "consul_client_details" {
-    value = <<EOT
+output "consul_client" {
+  value = <<EOT
  
-    Private IP : ${aws_instance.consul[1].private_ip} 
-    $ ssh -i ${aws_instance.consul[1].key_name}.pem ubuntu@${aws_instance.consul[1].public_ip}
+    Consul Client 1 :
+    ------------------ 
+        
+        Private IP : ${aws_instance.consul_client[0].private_ip}
+        SSH Login  : ssh -i ${aws_instance.consul_client[0].key_name}.pem ubuntu@${aws_instance.consul_client[0].public_ip}
 
-    Join Consul Server in Dev Mode via CLI:
-
-    consul agent -join ${aws_instance.consul[0].private_ip} -bind ${aws_instance.consul[1].private_ip} --data-dir /home/ubuntu/consul -hcl 'disable_remote_exec=false'
+    Consul Client 2 :
+    ------------------ 
+        
+        Private IP : ${aws_instance.consul_client[1].private_ip}
+        SSH Login  : ssh -i ${aws_instance.consul_client[1].key_name}.pem ubuntu@${aws_instance.consul_client[1].public_ip}
 
     EOT
 }
